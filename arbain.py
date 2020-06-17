e9c040934b49ee1ed27211749d41c94960713ac9
SantriInsyaf <aswadalansori@gmail.com>
2020-06-18 05:35:13 +0700

    initial commit

---
+++ arbain.py
@@ -0,0 +1,182 @@
+import pytz
+import os
+print ("\nARBAIN NAWAWI MP3 PLAYER BY ISALLKUN", end = '')
+print ("\nTerinspirasi dari BANGUNIN SAHUR-Will Pratama", end = '')
+print ("\nTHANKS TO SGB-TEAM & MAN IC PASURUAN", end = '')
+print ("\nSilahkan pilih bab yang ingin diputar", end = '')
+print ("\nBAB 1 NIAT", end = '')
+print ("\nBAB 2 IMAN, ISLAM, IHSAN", end = '')
+print ("\nBAB 3 FONDASI ISLAM", end = '')
+print ("\nBAB 4 PEMBENTUKAN MANUSIA", end = '')
+print ("\nBAB 5 BID'AH", end = '')
+print ("\nBAB 6 HALAL, HARAM, SUBHAT, DAN MUD'AMAH", end = '')
+print ("\nBAB 7 AGAMA DAN NASIHAT", end = '')
+print ("\nBAB 8 TERPELIHARA DARAH DAN HARTANYA", end = '')
+print ("\nBAB 9 PERINTAH & LARANGAN", end = '')
+print ("\nBAB 10 ALLAH ZAT YANG BAIK", end = '')
+print ("\nBAB 11 MENINGGALKAN KERAGUAN", end = '')
+print ("\nBAB 12 CIRI-CIRI KEBAIKAN ISLAM SESEORANG", end = '')
+print ("\nBAB 13 IMAN YANG SEMPURNA", end = '')
+print ("\nBAB 14 ORANG YANG HALAL DARAHNYA", end = '')
+print ("\nBAB 15 BERKATA BAIK ATAU DIAM", end = '')
+print ("\nBAB 16 TIDAK BOLEH MARAH", end = '')
+print ("\nBAB 17 BERBUAT BAIK TERHADAP SEGALA SESUATU", end = '')
+print ("\nBAB 18 TAQWA KEPADA ALLAH", end = '')
+print ("\nBAB 19 NASIHAT NABI MUHAMMAD SAW", end = '')
+print ("\nBAB 20 MALU", end = '')
+print ("\nBAB 21 ISTIQOMAH", end = '')
+print ("\nBAB 22 AMAL FARDHU DAPAT MASUK SURGA", end = '')
+print ("\nBAB 23 BACAAN DAN PERBUATAN YANG BERBOBOT", end = '')
+print ("\nBAB 24 RENUNGAN HADITS QUDSI", end = '')
+print ("\nBAB 25 AMALAN YANG BERNILAI SHODAQOH", end = '')
+print ("\nBAB 26 PERBUATAN YANG BERNILAI SHODAQOH", end = '')
+print ("\nBAB 27 KEBAIKAN DAN KEJELEKAN", end = '')
+print ("\nBAB 28 NASIHAT RASULULLAH SAW", end = '')
+print ("\nBAB 29 PERBUATAN YANG DAPAT MASUK SURGA DAN MENJAUHKAN DARI NERAKA", end = '')
+print ("\nBAB 30 LARANGAN MELEWATI BATAS", end = '')
+print ("\nBAB 31 PERBUATAN YANG DICINTAI ALLAH DAN MANUSIA", end = '')
+print ("\nBAB 32 LARANGAN BERBUAT SESUATU YANG BERBAHAYA", end = '')
+print ("\nBAB 33 MANFAAT BUKTI DAN SUMPAH ", end = '')
+print ("\nBAB 34 CARA MENGUBAH PERBUATAN MUNKAR", end = '')
+print ("\nBAB 35 LARANGAN BERWATAK BURUK", end = '')
+print ("\nBAB 36 BERBUAT BAIK DAN MANFAATNYA", end = '')
+print ("\nBAB 37 CATATAN ALLAH TENTANG PERBUATAN BAIK DAN BURUK", end = '')
+print ("\nBAB 38 KEHEBATAN WALI ALLAH", end = '')
+print ("\nBAB 39 SALAH, LUPA, DAN TERPAKSA", end = '')
+print ("\nBAB 40 BERGEGASLAH BERBUAT BAIK", end = '')
+print ("\nBAB 41 IMAN YANG SEMPURNA", end = '')
+print ("\nBAB 42 RENUNGAN HADIS QUDSI", end = '')
+print ("\nKETIK CTRL + C untuk menghentikan perulangan :)", end = '')
+print ("\n", end = '')
+
+arbain = str(input("Pilih Nomor: "))
+print ("\n", end = '')
+while True:
+	if "01" in arbain:
+		print ("\nBAB 1 NIAT", end = '')
+		os.system('mpv source/1.mp3')
+	if "02" in arbain:
+		print ("\nBAB 2 IMAN, ISLAM, IHSAN", end = '')
+		os.system('mpv source/2.mp3')
+	if "03" in arbain:
+		print ("\nBAB 3 FONDASI ISLAM", end = '')
+		os.system('mpv source/3.mp3')
+	if "04" in arbain:
+		print ("\nBAB 4 PEMBENTUKAN MANUSIA", end = '')
+		os.system('mpv source/4.mp3')
+	if "05" in arbain:
+		print ("\nBAB 5 BID'AH", end = '')
+		os.system('mpv source/5.mp3')
+	if "06" in arbain:
+		print ("\nBAB 6 HALAL, HARAM, SUBHAT, DAN MUD'AMAH", end = '')
+		os.system('mpv source/6.mp3')
+	if "07" in arbain:
+		print ("\nBAB 7 AGAMA DAN NASIHAT", end = '')
+		os.system('mpv source/7.mp3')
+	if "08" in arbain:
+		print ("\nBAB 8 TERPELIHARA DARAH DAN HARTANYA", end = '')
+		os.system('mpv source/8.mp3')
+	if "09" in arbain:
+		print ("\nBAB 9 PERINTAH DAN LARANGAN", end = '')
+		os.system('mpv source/9.mp3')
+	if "10" in arbain:
+		print ("\nBAB 10 ALLAH ZAT YANG BAIK", end = '')
+		os.system('mpv source/10.mp3')
+	if "11" in arbain:
+		print ("\nBAB 11 MENINGGALKAN KERAGUAN", end = '')
+		os.system('mpv source/11.mp3')	
+	if "12" in arbain:
+		print ("\nBAB 12 CIRI-CIRI KEBAIKAN ISLAM SESEORANG", end = '')
+		os.system('mpv source/12.mp3')
+	if "13" in arbain:
+		print ("\nBAB 13 MENINGGALKAN KERAGUAN", end = '')
+		os.system('mpv source/13.mp3')
+	if "14" in arbain:
+		print ("\nBAB 14 ORANG YANG HALAL DARAHNYA", end = '')
+		os.system('mpv source/14.mp3')
+	if "15" in arbain:
+		print ("\nBAB 15 BERKATA BAIK ATAU DIAM", end = '')
+		os.system('mpv source/15.mp3')
+	if "16" in arbain:
+		print ("\nBAB 16 TIDAK BOLEH MARAH", end = '')
+		os.system('mpv source/16.mp3')
+	if "17" in arbain:
+		print ("\nBAB 17 BERBUAT BAIK TERHADAP SEGALA SESUATU", end = '')
+		os.system('mpv source/17.mp3')
+	if "18" in arbain:
+		print ("\nBAB 18 TAQWA KEPADA ALLAH", end = '')
+		oS.system('mpv source/18.mp3')
+	if "19" in arbain:
+		print ("\nBAB 19 NASIHAT NABI MUHAMMAD SAW", end = '')
+		os.system('mpv source/19.mp3')
+	if "20" in arbain:
+		print ("\nBAB 20 MALU", end = '')
+		os.system('mpv source/20.mp3')
+	if "21" in arbain:
+		print ("\nBAB 21 ISTIQOMAH", end = '')
+		os.system('mpv source/21.mp3')
+	if "22" in arbain:
+		print ("\nBAB 22 AMAL FARDHU DAPAT MASUK SURGA", end = '')
+		os.system('mpv source/22.mp3')
+	if "23" in arbain:
+		print ("\nBAB 23 BACAAN DAN PERBUATAN YANG BERBOBOT", end = '')
+		os.system('mpv source/23.mp3')
+	if "24" in arbain:
+		print ("\nBAB 24 RENUNGAN HADITS QUDSI", end = '')
+		os.system('mpv source/24.mp3')
+	if "25" in arbain:
+		print ("\nBAB 25 AMALAN YANG BERNILAI SHODAQOH", end = '')
+		os.system('mpv source/25.mp3')
+	if "26" in arbain:
+		print ("\nBAB 26 PERBUATAN YANG BERNILAI SHODAQOH", end = '')		
+		os.system('mpv source/26.mp3')
+	if "27" in arbain:
+		print ("\nBAB 27 KEBAIKAN DAN KEJELEKAN", end = '')
+		os.system('mpv source/27.mp3')
+	if "28" in arbain:
+		print ("\nBAB 28 NASIHAT RASULULLAH SAW", end = '')
+		os.system('mpv source/28.mp3')
+	if "29" in arbain:
+		print ("\nBAB 29 PERBUATAN YANG DAPAT MASUK SURGA DAN MENJAUHKAN DARI NERAKA", end = '')
+		os.system('mpv source/29.mp3')
+	if "30" in arbain:
+		print ("\nBAB 30 LARANGAN MELEWATI BATAS", end = '')
+		os.system('mpv source/30.mp3')
+	if "31" in arbain:
+		print ("\nBAB 31 PERBUATAN YANG DICINTAI ALLAH DAN MANUSIA", end = '')
+		os.system('mpv source/31.mp3')
+	if "32" in arbain:
+		print ("\nBAB 32 LARANGAN BERBUAT SESUATU YANG BERBAHAYA", end = '')
+		os.system('mpv source/32.mp3')
+	if "33" in arbain:
+		print ("\nBAB 33 MANFAAT BUKTI DAN SUMPAH ", end = '')
+		os.system('mpv source/33.mp3')
+	if "34" in arbain:
+		print ("\nBAB 34 CARA MENGUBAH PERBUATAN MUNKAR", end = '')
+		os.system('mpv source/34.mp3')
+	if "35" in arbain:
+		print ("\nBAB 35 LARANGAN BERWATAK BURUK", end = '')
+		os.system('mpv source/35.mp3')
+	if "36" in arbain:
+		print ("\nBAB 36 BERBUAT BAIK DAN MANFAATNYA", end = '')
+		os.system('mpv source/36.mp3')
+	if "37" in arbain:
+		print ("\nBAB 37 CATATAN ALLAH TENTANG PERBUATAN BAIK DAN BURUK", end = '')
+		os.system('mpv source/37.mp3')
+	if "38" in arbain:
+		print ("\nBAB 38 KEHEBATAN WALI ALLAH", end = '')
+		os.system('mpv source/38.mp3')
+	if "39" in arbain:
+		print ("\nBAB 39 SALAH, LUPA, DAN TERPAKSA", end = '')
+		os.system('mpv source/39.mp3')
+	if "40" in arbain:
+		print ("\nBAB 40 BERGEGASLAH BERBUAT BAIK", end = '')
+		os.system('mpv source/40.mp3')
+	if "41" in arbain:
+		print ("\nBAB 41 IMAN YANG SEMPURNA", end = '')
+		os.system('mpv source/41.mp3')
+	if "42" in arbain:
+		print ("\nBAB 42 RENUNGAN HADIS QUDSI", end = '')
+		os.system('mpv source/42.mp3')
+else:
+	print("\nkamu keluar atau perintah yang dijalankan kosong", end = '')
